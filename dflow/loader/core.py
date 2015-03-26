4249d96aca09a16f89f8fbe1949a2f2b867a9fbd
Thomas S Hatch <thatch45@gmail.com>
2015-03-25 23:35:42 -0600

    notes for the loader

---
+++ dflow/loader/core.py
@@ -0,0 +1,9 @@
+'''
+Load plugins 
+'''
+
+# 1. loaded modules can plug into the data bus
+# 2. Modules don't need to plug into the data bus
+# 3. Many data buses can be suported
+# 4. data bus loaded modules have a default location on the bus
+
